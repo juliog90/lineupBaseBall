226ae1cace817c594d81d3cddd1936b9c70d4bc1
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-10-09 21:41:51 +0000

    Changing variables names to camelCase

--- models/League.php
+++ models/League.php
@@ -3,44 +3,44 @@
 class League
 {
 
-private $Id_Div;
-private $Name_Div;
+private $idDiv;
+private $nameDiv;
 
-public function getId_Div() { return $this->Id_Div; }
-public function getName_Div() { return $this->Name_Div: }
-public function setName_Div($Name_Div) { $this->Name_Div = $Name_Div; }
+public function getIdDiv() { return $this->IdDiv; }
+public function getNameDiv() { return $this->NameDiv: }
+public function setNameDiv($nameDiv) { $this->nameDiv = $nameDiv; }
 
 public function __construct() {
 if(func_num_args() == 0 ) {
-$this->Id_Div = 0;
-$this->Name_Div = "";
+$this->idDiv = 0;
+$this->nameDiv = "";
 }
 
 if(func_num_args() == 1) {
 $connection = MySqlConnection::getConnection();
-$query = 'select Id_Div, Name_Div from Division where Id_Div = ?';
+$query = 'select idDiv, nameDiv from Division where idDiv = ?';
 $command = $connection->prepare($query);
 $command-> bind_param('s', func_get_arg(0));
 $command->execute();
-$command->bind_result($Id_Div, $Name_Div);
+$command->bind_result($idDiv, $nameDiv);
 if($command->fetch()) {
-$this->Id_Div = $Id_Div;
-$this->Name_Div = $Name_Div;
+$this->idDiv = $idDiv;
+$this->nameDiv = $nameDiv;
 } 
 else 
 throw new RecordNotFoundException(func_get_arg(0));
 }
 
 if(func_num_args() == 2) {
-$this->Id_Div = func_get_arg(0);
-$this->Name_Div = func_get_arg(1);
+$this->idDiv = func_get_arg(0);
+$this->nameDiv = func_get_arg(1);
 }
 }
 
 public function toJson() {
 return json_encode (array(
-'Id_Div'=>$this->Id_Div,
-'Name_Div'=>$this->Name_Div
+'idDiv'=>$this->idDiv,
+'nameDiv'=>$this->nameDiv
 ));
 }
 }
