2ba9292a09f92b5ed64a27d8f333befaa788673d
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-10-09 22:00:27 +0000

    bug fix

--- models/League.php
+++ models/League.php
@@ -7,7 +7,7 @@ private $idDiv;
 private $nameDiv;
 
 public function getIdDiv() { return $this->idDiv; }
-public function getNameDiv() { return $this->nameDiv: }
+public function getNameDiv() { return $this->nameDiv; }
 public function setNameDiv($nameDiv) { $this->nameDiv = $nameDiv; }
 
 public function __construct() {
