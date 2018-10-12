bb6a3ab0fbc2db2486a1a929b3239b423f303336
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-10-12 19:47:43 +0000

    constructor de la base de datos arreglado

--- models/Players.php
+++ models/Players.php
@@ -1,22 +1,25 @@
 <?php
-    class Players {
+
+require_once('connection.php');
+
+    class Player {
         private $id;
-        private $name; 
-        private $launching;
-        private $age;
+        private $firstName; 
+        private $lastName; 
+        private $dateOfBirth;
         private $heigth;
         private $weigth;
+        private $team;
 
         public function getId() { return $this->id; }
+        public function getFirstName() { return $this->firstName; }
+        public function setFirstName($firstName) { $this->firstName = $firstName; }
 
-        public function getName() { return $this->name; }
-        public function setName($name) { $this->name = $name; }
+        public function getLastName() { return $this->lastName; }
+        public function setLastName($lastName) { $this->lastName = $lastName; }
 
-        public function getPosition() { return $this->position; }
-        public function setPosition($position) { $this->position = $position; }
-
-        public function getAge() { return $this->age; }
-        public function setAge($age) { $this->age = $age; }
+        public function getDateOfBirth() { return $this->dateOfBirth; }
+        public function setDateOfBirth($dateOfBirth) { $this->dateOfBirth = $dateOfBirth; }
 
         public function getHeigth() { return $this->heigth; }
         public function setHeigth($heigth) { $this->heigth = $heigth; }
@@ -27,27 +30,30 @@
         public function __construct() {
             if(func_num_args() == 0) {
                 $this->id = 0;
-                $this->name = "";
-                $this->launching = "";
-                $this->age = 0;
+                $this->firstName = "";
+                $this->lastName = "";
+                $this->team = "";
+                $this->dateOfBirth = 0;
                 $this->heigth = "";
                 $this->weigth = "";
             }
 
             if(func_num_args() == 1) {
                 $connection = MySqlConnection::getConnection();
-                $query = 'select Id_Div, Name_Div from Division where Id_Div = ?';
+                $query = 'select first_name, last_name, id_p, date_of_birth, Height, Weight_p, id_T from player where id_p = ?';
                 $command = $connection->prepare($query);
-                $command-> bind_param('s', func_get_arg(0));
+                $id = func_get_arg(0);
+                $command->bind_param('s', $id);
                 $command->execute();
-                $command->bind_result($id, $name, $launching, $age, $heigth, $weigth);
+                $command->bind_result($first_name, $last_name, $id_p, $date_of_birth, $Heigth, $Weigth_p, $id_T);
                 if($command->fetch()) {
-                    $this->id = $id;
-                    $this->name = $name;
-                    $this->launching = $launching;
-                    $this->age = $age;
-                    $this->heigth = $heigth;
-                    $this->weigth = $weigth;
+                    $this->id = $id_p;
+                    $this->firstName = $first_name;
+                    $this->lastName = $last_name;
+                    $this->team = $id_T;
+                    $this->dateOfBirth = $date_of_birth;
+                    $this->heigth = $Heigth;
+                    $this->weigth = $Weigth_p;
                 } 
                 else 
                     throw new RecordNotFoundException(func_get_arg(0));
@@ -55,20 +61,22 @@
 
             if(func_num_args() == 6) {
                 $this->id = func_get_arg(0);
-                $this->name = func_get_arg(1);
-                $this->launching = func_get_arg(2);
-                $this->age = func_get_arg(3);
+                $this->firstName = func_get_arg(1);
+                $this->team = func_get_arg(2);
+                $this->dateOfBirth = func_get_arg(3);
                 $this->heigth = func_get_arg(4);
                 $this->weigth = func_get_arg(5);
+                $this->lastName = func_get_arg(6);
             }
         }
 
         public function toJson() {
             return json_encode(array(
                 'id'=>$this->id,
-                'name'=>$this->name,
-                'launching'=>$this->launching,
-                'age'=>$this->age,
+                'firstName'=>$this->firstName,
+                'lastName'=>$this->lastName,
+                'team'=>$this->team,
+                'dateOfBirth'=>$this->dateOfBirth,
                 'heigth'=>$this->heigth,
                 'weigth'=>$this->weigth
             ));
