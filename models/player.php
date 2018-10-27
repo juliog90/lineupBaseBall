6e1ec79145c78107e6290c0a0e9f11f13c6a7a21
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-10-27 19:03:43 +0000

    borrando nombre y apellido

--- models/player.php
+++ models/player.php
@@ -9,8 +9,6 @@ require_once('exceptions/recordnotfoundexception.php');
         private $id;
         private $team;
         private $nickname;
-        private $firstName; 
-        private $lastName; 
         private $birthdate;
         private $debut;
         private $image;
@@ -24,12 +22,6 @@ require_once('exceptions/recordnotfoundexception.php');
 
         public function getNickname() { return $this->nickname; }
         public function setNickname($nickname) { $this->nickname = $nickname; }
-
-        public function getFirstName() { return $this->firstName; }
-        public function setFirstName($firstName) { $this->firstName = $firstName; }
-
-        public function getLastName() { return $this->lastName; }
-        public function setLastName($lastName) { $this->lastName = $lastName; }
 
         public function getBirthdate() { return $this->birthdate; }
         public function setBirthdate($birthdate) { $this->birthdate = $birthdate; }
@@ -57,8 +49,6 @@ require_once('exceptions/recordnotfoundexception.php');
                 $this->id = 0;
                 $this->team = new Team();
                 $this->nickname = "";
-                $this->firstName = "";
-                $this->lastName = "";
                 $this->birthdate = new DateTime();
                 $this->debut = new DateTime();
                 $this->image = "";
@@ -73,13 +63,11 @@ require_once('exceptions/recordnotfoundexception.php');
                 $idTemp = func_get_arg(0);
                 $command->bind_param('i', $idTemp);
                 $command->execute();
-                $command->bind_result($id, $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number, $stats);
+                $command->bind_result($id, $team, $nickname, $birthdate, $debut, $image, $number, $stats);
                 if($command->fetch()) {
                     $this->id = $id;
                     $this->team = new Team($team);
                     $this->nickname = $nickname;
-                    $this->firstName = $firstName;
-                    $this->lastName = $lastName;
                     $this->birthdate = DateTime::createFromFormat('Y-m-d', $birthdate);
                     $this->debut = DateTime::createFromFormat('Y-m-d', $debut);
                     $this->image = $image;
@@ -97,8 +85,6 @@ require_once('exceptions/recordnotfoundexception.php');
                 $this->id = func_get_arg(0);
                 $this->team = new Team(func_get_arg(1));
                 $this->nickname = func_get_arg(2);
-                $this->firstName = func_get_arg(3);
-                $this->lastName = func_get_arg(4);
                 $this->birthdate = func_get_arg(5);
                 $this->debut = func_get_arg(6);
                 $this->image = func_get_arg(7);
@@ -114,10 +100,10 @@ require_once('exceptions/recordnotfoundexception.php');
             $query = 'getAllPlayers()';
             $command = $connecion->prepare($query);
             $command->execute();  
-            $command->bind_result($id, $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number, $stats);
+            $command->bind_result($id, $team, $nickname, $birthdate, $debut, $image, $number, $stats);
             while($command->fetch())
             {
-                array_push($players, new Player($id, $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number, $stats));
+                array_push($players, new Player($id, $team, $nickname, $birthdate, $debut, $image, $number, $stats));
             }
 
             mysqli_stmt_close($command);
@@ -142,20 +128,18 @@ require_once('exceptions/recordnotfoundexception.php');
         public function edit()
         {
             $connection = MySqlConnection::getConnection(); 
-            $statement = 'editCategory(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+            $statement = 'editPlayer(?, ?, ?, ?, ?, ?, ?, ?)';
             $command = $connection->prepare($statement);
             $id = $this->id;
             $team = $this->team->id;
             $nickname = $this->nickname;
-            $firstName = $this->firstName;
-            $lastName = $this->lastName;
             $birthdate = $createFromFormat('Y-m-d', $this->birthdate);
             $debut = $createFromFormat('Y-m-d', $this->debut);
             $image = $this->image;
-            $lastName = $this->lastName;
-            $lastName = $this->lastName;
+            $number = $this->number;
+            $stats = $this->stats;
 
-            $command->bind_param('i', $id);
+            $command->bind_param('iissssii', $id, $team, $nickname, $birthdate, $debut, $image, $number, $stats);
             $result = $command->execute();
 
             mysqli_stmt_close($command);
@@ -165,18 +149,16 @@ require_once('exceptions/recordnotfoundexception.php');
         public function add()
         {
             $connection = MySqlServerConnection::getConnection();
-            $statement = 'addPlayer(?, ?, ?, ?, ?, ?, ?, ?, ?)';
+            $statement = 'addPlayer(?, ?, ?, ?, ?, ?, ?)';
             $id = $this->id;
             $team = $this->team->id;
             $nickname = $this->nickname;
-            $firstName = $this->firstName;
-            $lastName = $this->lastName;
             $birthdate = $createFromFormat('Y-m-d', $this->birthdate);
             $debut = $createFromFormat('Y-m-d', $this->debut);
             $image = $this->image;
             $number = $this->number;
             $stats = $this->stats;
-            $command->bind_param('issssssii',  $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number, $stats);
+            $command->bind_param('issssii',  $team, $nickname, $birthdate, $debut, $image, $number, $stats);
             $command = $connection->prepare($statement);
             $result = $command->execute();
             mysqli_stmt_close($command);
@@ -204,8 +186,6 @@ require_once('exceptions/recordnotfoundexception.php');
                 'id'=>$this->id,
                 'team'=> json_decode($this->team->toJson()),
                 'nickname'=>$this->nickname,
-                'firstName'=>$this->firstName,
-                'lastName'=>$this->lastName,
                 'birthdate'=>$this->birthdate->format('Y-m-d'),
                 'debut'=>$this->debut->format('Y-m-d'),
                 'image'=>$this->image,
