dacd8e7ffc09d248711bcddf9057baaeae95876a
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-10-30 20:10:23 +0000

    inserts de prueba

---
+++ database/insert_categories.sql
@@ -0,0 +1,1 @@
+insert into categories(catName) values('Biberon'),('Zapeta'),('Escuelita'),('Peewee'),('Infantil'),('Pony'),('Colt'),('Juvenil');
---
+++ database/insert_teams.sql
@@ -0,0 +1,5 @@
+insert into teams(staId, teaName, catId, coaId) values
+(1, 'The Hitmen', 1, 1),
+(1, 'Sliders', 1, 2),
+(1, 'Boomers', 2, 3),
+(1, 'Rhinos', 2, 4);
