8638ac1b09d2866c80c9246ec0366a2f7c061840
Julio Gastelum <tjuliogastelum90@gmail.com>
2018-11-04 20:34:41 -0800

    using baseball system player class

--- models/player.php
+++ models/player.php
@@ -1,211 +1,172 @@
 <?php
-    
-require_once('connection.php');
-require_once('team.php');
-require_once 'models/playerstats.php';
-require_once('exceptions/recordnotfoundexception.php');
+    require_once('ConnectionMySql.php');
+    require_once("Person.php");
+    require_once('team.php');
 
-class Player {
+    //require_once("team.php"); cuando haya team
+    class Player
+    {
+        #attributes
+        private $id;
+        private $person;
+        private $status;
+        private $team;
+        private $nickName;
+        private $birthDate;
+        private $debut;
+        private $image;
+        private $playerNumber;
 
-    private $id;
-    private $status;
-    private $team;
-    private $nickname;
-    private $firstName; 
-    private $lastName; 
-    private $birthdate;
-    private $debut;
-    private $image;
-    private $number;
+        #properties
+        public function getId(){return $this->id;}
+        public function setId($id){$this->id = $id;}
 
-    public function getId() { return $this->id; }
+        public function getPerson(){return $this->person;}
+        public function setPerson($person){$this->person = $person;}
+        
+        public function getStatus(){return $this->status;}
+        public function setStatus($status){$this->status = $status;}
+        
+        public function getTeam(){return $this->team;}
+        public function setTeam($team){$this->team = $team;}
+        
+        public function getNickName(){return $this->nickName;}
+        public function setNickName($nickName){$this->nickName = $nickName;}
+        
+        public function getBirthDate(){return $this->birthDate;}
+        public function setBirthDate($birthDate){$this->birthDate = $birthDate;}
+        
+        public function getDebut(){return $this->debut;}
+        public function setDebut($debut){$this->debut = $debut;}
+        
+        public function getImage(){return $this->image;}
+        public function setImage($image){$this->image = $image;}
+        
+        public function getPlayerNumber(){return $this->playerName;}
+        public function setPlayerNumber($playerNumber){$this->playerName = $playerNumber;}
 
-    public function getTeam() { return $this->team; }
-    public function setTeam($team) { $this->team = $team; }
-
-    public function getStatus() { return $this->status; }
-    public function setStatus($status) { $this->status = $status; }
-
-    public function getNickname() { return $this->nickname; }
-    public function setNickname($nickname) { $this->nickname = $nickname; }
-
-    public function getFirstName() { return $this->firstName; }
-    public function setFirstName($firstName) { $this->firstName = $firstName; }
-
-    public function getLastName() { return $this->lastName; }
-    public function setLastName($lastName) { $this->lastName = $lastName; }
-
-    public function getBirthdate() { return $this->birthdate; }
-    public function setBirthdate($birthdate) { $this->birthdate = $birthdate; }
-
-    public function getDebut() { return $this->debut; }
-    public function setDebut($debut) { $this->debut = $debut; }
-
-    public function getImage() { return $this->image; }
-    public function setImage($image) { $this->image = $image; }
-
-    public function getNumber() { return $this->number; }
-    public function setNumber($number) { $this->number = $number; }
-
-    public function __construct() {
-        if(func_num_args() == 0) {
-            $this->id = 0;
-            $this->team = new Team();
-            $this->nickname = "";
-            $this->firstName = "";
-            $this->lastName = "";
-            $this->birthdate = new DateTime();
-            $this->debut = new DateTime();
-            $this->image = "";
-            $this->number = 0;
+        #constructor
+        public function __construct(){
+            if(func_num_args() == 0){
+                $this->id = "";
+                $this->person = new Person();
+                $this->status = "";
+                $this->team = new Team();
+                $this->nickName = "";
+                $this->birthDate = "";
+                $this->debut = "";
+                $this->image = "";
+                $this->playerNumber = "";
             }
 
-        if(func_num_args() == 1) {
-            $connection = MySqlConnection::getConnection();
-            $query = 'select pl.plaId, pl.teaId, plaNickname, p.perFirstName, p.perLastName, plaBirthdate, plaDebut, plaImage, plaNumber 
-                      from persons as p inner join players as pl on p.perId = pl.perId where pl.plaId = ?';
-            $command = $connection->prepare($query);
-            $idTemp = func_get_arg(0);
-            $command->bind_param('i', $idTemp);
-            $command->execute();
-            $command->bind_result($id, $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number);
-            if($command->fetch()) {
-                $this->id = $id;
-                $this->team = new Team($team);
-                $this->nickname = $nickname;
-                $this->firstName = $firstName;
-                $this->lastName = $lastName;
-                $this->birthdate = DateTime::createFromFormat('Y-m-d', $birthdate);
-                $this->debut = DateTime::createFromFormat('Y-m-d', $debut);
-                $this->image = $image;
-                $this->number = $number;
-            } 
-                else 
-                    throw new RecordNotFoundException(func_get_arg(0));
-
-                mysqli_stmt_close($command);
-                $connection->close();
-
+            if(func_num_args() == 1)
+            {
+                $connection = MySqlConnection::Connection();
+                $query = "select * from players where plaId = ?";
+                $command = $connection->prepare($query);
+                $command->bind_param('i',func_get_arg(0));
+                $command->execute();
+                $command->bind_result($id,$person, $status, $team, $nickName, $birthDate, $debut, $image, $playerNumber);
+                if($command->fetch())
+                {
+                    $this->id = $id;
+                    $this->person = $person;
+                    $this->status = $status;
+                    $this->team = $team;
+                    $this->nickName = $nickName;
+                    $this->birthDate = $birthDate;
+                    $this->debut = $debut;
+                    $this->image = $image;
+                    $this->playerNumber = $playerNumber;
+                }
+            }
+            if(func_num_args() == 9){
+                $this->id = func_get_arg(0);
+                $this->person = func_get_arg(1);
+                $this->status = func_get_arg(2);
+                $this->team = func_get_arg(3);
+                $this->nickName = func_get_arg(4);
+                $this->birthDate = func_get_arg(5);
+                $this->debut = func_get_arg(6);
+                $this->image = func_get_arg(7);
+                $this->playerNumber = func_get_arg(8);
+            }
         }
-
-
-
-        if(func_num_args() == 10) {
-            $this->id = func_get_arg(0);
-            $this->status = func_get_arg(1);
-            $this->team = new Team(func_get_arg(2));
-            $this->nickname = func_get_arg(3);
-            $this->firstName = func_get_arg(3);
-            $this->lastName = func_get_arg(5);
-            $this->birthdate = func_get_arg(6);
-            $this->debut = func_get_arg(7);
-            $this->image = func_get_arg(8);
-            $this->number = func_get_arg(9);
-        }
-    }
-
-    public static function getAll()
-    {
-        $players = array();
-        $connection = MySqlServerConnection::getConnection();
-        $query = 'getAllPlayers()';
-        $command = $connecion->prepare($query);
-        $command->execute();  
-        $command->bind_result($id, $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number);
-        while($command->fetch())
-        {
-            array_push($players, new Player($id, $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number));
-        }
-
-        mysqli_stmt_close($command);
-        $connection->close();
-
-        return $players;
-    }
-
-    public function remove()
-    {
-        $connection = MySqlConnection::getConnection(); 
-        $statement = 'rmPlayer(?)';    
-        $command = $connection->prepare($statement);
-        $id = $this->id;
-        $command->bind_param('i', $id);
-        $result = $command->execute();
-
-        mysqli_stmt_close($command);
-        $connection->close();
-    }
-
-    public function edit()
-    {
-        $connection = MySqlConnection::getConnection(); 
-        $statement = 'editCategory(?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $command = $connection->prepare($statement);
-        $id = $this->id;
-        $team = $this->team->id;
-        $nickname = $this->nickname;
-        $firstName = $this->firstName;
-        $lastName = $this->lastName;
-        $birthdate = $createFromFormat('Y-m-d', $this->birthdate);
-        $debut = $createFromFormat('Y-m-d', $this->debut);
-        $image = $this->image;
-        $number = $this->number;
-
-        $command->bind_param('i', $id);
-        $result = $command->execute();
-
-        mysqli_stmt_close($command);
-        $connection->close();
-    }
-        
-    public function add()
-    {
-        $connection = MySqlServerConnection::getConnection();
-        $statement = 'addPlayer(?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        $id = $this->id;
-        $team = $this->team->id;
-        $nickname = $this->nickname;
-        $firstName = $this->firstName;
-        $lastName = $this->lastName;
-        $birthdate = $createFromFormat('Y-m-d', $this->birthdate);
-        $debut = $createFromFormat('Y-m-d', $this->debut);
-        $image = $this->image;
-        $number = $this->number;
-        $command->bind_param('issssssii',  $team, $nickname, $firstName, $lastName, $birthdate, $debut, $image, $number);
-        $command = $connection->prepare($statement);
-        $result = $command->execute();
-        mysqli_stmt_close($command);
-        $connection->close();
-
-        return $result;
-    }
-
-    public static function getAllToJson()
-    {
-        $playersJson = array();
-        $players = self::getAll();
-
-        foreach ($players as $value) {
-            array_push($playersJson, json_decode($value->toJson()));    
-        }
-
+        #methods
+        public function toJson(){
             return json_encode(array(
-                'players' => json_decode($playersJson)
+                'id' => $this->id,
+                'person'=>json_decode($this->person->toJson()),
+                'status'=>$this->status,
+                'team'=>$this->team,
+                'team'=>json_decode($this->team->toJson()),
+                'nickName'=>$this->nickName,
+                'birthDate'=>$this->birthDate,
+                'debut'=>$this->debut,
+                'image'=>$this->image,
+                'number'=>$this->playerNumber
             ));
         }
 
-        public function toJson() {
-            return json_encode(array(
-            'id'=>$this->id,
-            'team'=> json_decode($this->team->toJson()),
-            'nickname'=>$this->nickname,
-            'firstName'=>$this->firstName,
-            'lastName'=>$this->lastName,
-            'birthdate'=>$this->birthdate,
-            'debut'=>$this->debut,
-            'image'=>$this->image,
-            'number'=>$this->number
-        ));
+        public function add()
+        {
+            $connection = MySqlConnection::Connection();
+            $query = "Insert Into players (perId, staId, teaId, plaNickname, plaBirthdate, plaDebut, plaImage, plaNumber) Values (?,?,?,?,?,?,?,?)"; 
+            $command = $connection->prepare($query);
+            $command->bind_param('iiissssi', $this->person, $this->status, $this->team, $this->nickName, $this->birthDate, $this->debut, $this->image, $this->playerNumber);
+            $result = $command->execute();
+            mysqli_stmt_close($command);
+            $connection->close();
+            return $result;
+        }
+
+        public function edit()
+        {
+            $connection = MySqlConnection::Connection();
+            $query = "update players set perId = ?, staId = ?, teaId = ?, plaNickname = ?, plaBirthdate = ?, plaDebut = ?, plaImage = ?, plaNumber = ?";
+            $command = $connection->prepare($query);
+            $command->bind_param('iiissssi', $this->person, $this->status, $this->team, $this->nickName, $this->birthDate, $this->debut, $this->image, $this->playerNumber);
+            $result = $command->execute();
+            mysqli_stmt_close($command);
+            $connection->close();
+            return $result;
+        }
+
+        public function delete()
+        {
+            $connection = MySqlConnection::Connection();
+            $query = 'delete from players where id = ?';
+            $command = $connection->prepare($query);
+            $command->bind_param('i',$this->id);
+            $result = $command->execute();
+            mysqli_stmt_close($command);
+            $connection->close();
+            return $result;
+        }
+
+        public static function getAll()
+        {
+            $list = array();
+            $connection = MySqlConnection::Connection();
+            $query='select * from players';
+            $command = $connection->prepare($query);
+            $command->execute();
+            $command->bind_result($id, $person, $status, $team, $nickName, $birthDate, $debut, $image, $playerNumber);
+            while($command->fetch())
+            {
+                array_push($list, new Player($id,$person,$status,$team, $nickName, $birthDate, $debut, $image, $playerNumber));
+            }
+            return $list;
+        }
+
+        public static function getAllToJson()
+        {
+            $jsonArray = array();
+            foreach(self::getAll() as $item)
+            {
+                array_push($jsonArray, json_decode($item->toJson()));
+            }
+            return json_encode($jsonArray);
+        }
     }
-}
 ?>
